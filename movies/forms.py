f1d4c0e1246ca25e2a59e42c0f5c0988053b04a4
almat-kst <almat_1005@mail.ru>
2021-05-04 16:03:56 +0600

    finished

---
+++ movies/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Reviews, Rating, RatingStar
+
+class ReviewForm(forms.ModelForm):
+    """Отзывы и коментарии"""
+    class Meta:
+        model = Reviews
+        fields = ("name", "email", "text")
+
+class RatingForm(forms.ModelForm):
+    star = forms.ModelChoiceField(
+        queryset=RatingStar.objects.all(), widget=forms.RadioSelect(), empty_label=None
+    )
+
+    class Meta:
+        model = Rating
+        fields = ("star",)
